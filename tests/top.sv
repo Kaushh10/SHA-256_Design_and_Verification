59ee8bd9c8898812ed0f3e793afc02d7aac6f0aa
sachin kumar <sachkr24@gmail.com>
2026-01-29 14:05:46 +0530

    Update top.sv

--- tests/top.sv
+++ tests/top.sv
@@ -1,28 +1,34 @@
-module top;
+`timescale 1ns / 1ps
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+`include "test.sv"
 
-    logic clk;
-    logic rst_n;
-    hash256 sif(clk);
+module top_tb;
 
-    hash_core DUT(.clk(clk),.rst_n(sif.rst_n),.Kt_i(sif.Kt_i),.Wt_i(sif.Wt_i),
-         .A_i(sif.A_i),.B_i(sif.B_i),.C_i(sif.C_i),.D_i(sif.D_i),.E_i(sif.E_i),.F_i(sif.F_i),.G_i(sif.G_i),.H_i(sif.H_i),
-         .A_o(sif.A_o),.B_o(sif.B_o),.C_o(sif.C_o),.D_o(sif.D_o),.E_o(sif.E_o),.F_o(sif.F_o),.G_o(sif.G_o),.H_o(sif.H_o),
-          .finall(sif.finall),.done(sif.done));
+bit clk;
 
 
-    initial clk = 0;
-    always #5 clk = ~clk;
+initial
+forever #5 clk = ~clk;
 
-    initial begin
-    rst_n = 0;
-    #20 rst_n =1;
-    end
-    
-    initial begin
-    uvm_config_db#(virtual hash256)::set(null,"env.agt.drv","vif",sif);
-    uvm_config_db#(virtual hash256)::set(null,"env.agt.mon","vif",sif);
-            run_test("sha256_test");
+sha256_if sif(.clk(clk));
 
-     end
-endmodule
+
+top DUT (.clk(clk),
+         .rst_n(sif.rst_n),
+         .msg_valid(sif.msg_valid),
+         .byte_valid(sif.byte_valid),
+         .msg_word(sif.data_in),
+         .hash_done(sif.hash_done),
+         .fin_hash(sif.fin_hash)
+         );
         
+
+initial begin
+uvm_config_db#(virtual sha256_if)::set(null,"*","vif",sif);
+run_test("sha_test");
+end
+
+
+
+endmodule
