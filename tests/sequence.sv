6ee5b4dc394544d4b11bcaab1edd926ab94ca291
sachin kumar <sachkr24@gmail.com>
2026-01-29 18:32:27 +0530

    Update sequence.sv

--- tests/sequence.sv
+++ tests/sequence.sv
@@ -5,23 +5,24 @@ class sha256_sequence extends uvm_sequence #(sha256_sequence_item);
     
     `uvm_object_utils(sha256_sequence)
     
+    
     function new(string name ="sha256_sequence");
         super.new(name);
     endfunction
     
     virtual task body();
-        sha256_sequence_item req();
+        sha256_sequence_item req;
         
     repeat(1)begin
-        req = sha256_sequence_item::type_id::create("req");
+        
         
         start_item(req);
-        
+        req = sha256_sequence_item::type_id::create("req");
       if(!req.randomize() with {rst_n == 1;})begin
             `uvm_error("SEQ","Randomization failed")
          end
          
-         finish_item();
+         finish_item(req);
          
       end
    endtask
@@ -37,18 +38,18 @@ class sha256_sequence extends uvm_sequence #(sha256_sequence_item);
     endfunction
     
     virtual task body();
-        sha256_sequence_item req();
+        sha256_sequence_item req;
         
     repeat(1)begin
-        req = sha256_sequence_item::type_id::create("req");
+        
         
         start_item(req);
-        
+        req = sha256_sequence_item::type_id::create("req");
       if(!req.randomize() with {rst_n == 0;})begin
             `uvm_error("SEQ","Randomization failed")
          end
          
-         finish_item();
+         finish_item(req);
          
       end
    endtask
