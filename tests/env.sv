7df86e3673c4b96c3043254d521d30918affe4f7
sachin kumar <sachkr24@gmail.com>
2026-01-29 14:04:15 +0530

    Update env.sv

--- tests/env.sv
+++ tests/env.sv
@@ -2,23 +2,27 @@
 import uvm_pkg::*;
 
 class sha256_env extends uvm_env;
-    sha256_agent agt;
-    sha256_scoreboard scb;
+
+`uvm_component_utils(sha256_env)
+
+    sha256_agent agnt;
+    sha256_scoreboard sb;
+    uvm_sequencer #(sha256_sequence_item) seqr;
     
-    `uvm_component_utils(sha256_env)
-    
-    function new(string name = "sha256_env",uvm_component parent);
-            super.new(name,parent);
+    function new(string name = "sha256_env",uvm_component parent = null);
+        super.new(name,parent);
     endfunction
     
-    
     function void build_phase(uvm_phase phase);
-            super.build_phase(phase);
-       agt = sha256_agent::type_id::create("agt",this);
-       scb = sha256_scoreboard::type_id::create("scb",this);
-       
-      agt.mon.anp.connect(scb.imp);
-     endfunction
-   endclass
-   
-       
+        agnt = sha256_agent::type_id::create("agnt",this);
+        sb = sha256_scoreboard::type_id::create("sb",this);
+        endfunction
+        
+     function void connect_phase(uvm_phase phase);
+        drv.seq_item_port.connect(seqr.seq_item_export);
+        mon.mon_ap.connect(sb.sb_ap);
+    endfunction
+    endclass
+    
+    
+    
